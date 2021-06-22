9535540721d06778621ab9a2ccc15902439fe6cd
Peter Bwire <bwire517@gmail.com>
2021-06-22 20:20:36 +0300

    Switch to the normal way using client options

--- client_setup.go
+++ client_setup.go
@@ -2,23 +2,73 @@ package file_v1
 
 import (
 	"context"
-	"golang.org/x/oauth2/clientcredentials"
+	apic "github.com/antinvestor/apis"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
+const ctxKeyService = "filesClientKey"
 
+func defaultFilesClientOptions() []apic.ClientOption {
+	return []apic.ClientOption{
+		apic.WithEndpoint("https://files.api.antinvestor.com"),
+	}
+}
 
-func NewClient(ctx context.Context, clientId string, clientSecret string, tokenUrl string) *APIClient {
+func ToContext(ctx context.Context, fileClient *FilesClient) context.Context {
+	return context.WithValue(ctx, ctxKeyService, fileClient)
+}
 
-	cfg := &clientcredentials.Config{
-		ClientID:    clientId,
-		ClientSecret: clientSecret,
-		TokenURL:     tokenUrl,
+func FromContext(ctx context.Context) *FilesClient {
+	fileClient, ok := ctx.Value(ctxKeyService).(*FilesClient)
+	if !ok {
+		return nil
 	}
 
-	cfg.Client(ctx)
+	return fileClient
+}
+
+// FilesClient is a client for interacting with the file service API.
+// Methods, except Close, may be called concurrently. However,
+//fields must not be modified concurrently with method calls.
+type FilesClient struct {
+	APIClient
+	// The x-ant-* metadata to be sent with each request.
+	xMetadata metadata.MD
+}
+
+// NewFilesClient creates a new file client.
+// The service that an application uses to create and read files stored in the system
+func NewFilesClient(ctx context.Context, opts ...apic.ClientOption) (*FilesClient, error) {
+	clientOpts := defaultFilesClientOptions()
+
+	httClient, err := apic.HttpClient(ctx, append(clientOpts, opts...)...)
+
+	if err != nil {
+		return nil, err
+	}
 
 	configuration := NewConfiguration()
-	configuration.HTTPClient = cfg.Client(ctx)
+	configuration.HTTPClient = httClient
 
-	return NewAPIClient(configuration)
+	cl := &FilesClient{}
+	cl.cfg = configuration
+	cl.common.client = &cl.APIClient
+	cl.DefaultApi = (*DefaultApiService)(&cl.common)
+
+	cl.setClientInfo()
+
+	return cl, nil
 }
+
+// setClientInfo sets the name and version of the application in
+// the `x-goog-api-client` header passed on each request. Intended for
+// use by Google-written clients.
+func (fc *FilesClient) setClientInfo(keyval ...string) {
+	kv := append([]string{"gl-go", apic.VersionGo()}, keyval...)
+	kv = append(kv, "grpc", grpc.Version)
+	fc.xMetadata = metadata.Pairs("x-ai-api-client", apic.XAntHeader(kv...))
+}
+
+
+
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/antinvestor/service-files-api
 
 go 1.16
 
-require golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
+require (
+	github.com/antinvestor/apis v1.0.5
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
+	google.golang.org/grpc v1.35.0
+)
